6f8c95ab5ea153ad0da3c34d680c8a4594509cb2
Amala Shwetah <93658002+Amala02@users.noreply.github.com>
2023-08-20 15:06:17 +0530

    commit1

--- src/App2.js
+++ src/App2.js
@@ -3,7 +3,7 @@ import React from "react";
 import './App.css';
 import image from './web-img.jpg';
 import { styled } from '@mui/material/styles';
-import Image2 from './TechFest.jpg';
+
 import Home from ".";
 import {BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-router-dom';
 function App() {
@@ -16,18 +16,16 @@ function App() {
       <div id= "icon-contact">icon</div>
       </div>
 
-    <div class="Container">
-        <div id="welcome">Welcome to Techfest 2k22.</div>
-        <div id="image-box">
-        <img src={Image2}></img>
-        </div>
-        <div id="text-box">
-            <p>"Lorem ipsum dolor sit amet,<br></br> consectetur adipiscing elit,<br></br>sed do eiusmod tempor<br></br> incididunt ut labore et <br></br></p>
+    <div class="left">
+    <img src={image}/>
+    </div>
+    <div class="info">
+      <h1>TechZar</h1>
+      <p>"Lorem ipsum dolor sit amet,<br></br> consectetur adipiscing elit,<br></br>sed do eiusmod tempor<br></br> incididunt ut labore et <br></br></p>
 
-        </div>
-        <div id="explore">
-           <button class="btn"> Explore events</button>
-        </div>
+      <button >
+        EVENTS
+      </button>
     </div>
   </div>
   );
